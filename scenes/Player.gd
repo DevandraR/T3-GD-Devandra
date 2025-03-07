77bbc866ebc5f8507e170ca9a113e3a3f84cfc35
DevandraR <devandra271203@gmail.com>
2025-03-07 09:37:00 +0700

    run gdformat local tutorial 3

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -8,12 +8,13 @@ var jump_counter
 
 @onready var sprite = $AnimatedSprite2D
 
+
 func _physics_process(delta):
 	velocity.y += delta * gravity
-	
+
 	if is_on_floor:
 		jump_counter = 0
-	
+
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -walk_speed
 		sprite.play("walk")  # Play walk animation
@@ -25,7 +26,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 		sprite.play("default")
-		
+
 	if Input.is_action_pressed("ui_left") and Input.is_action_pressed("dash"):
 		velocity.x = -walk_speed - 200
 		sprite.play("walk")  # Play walk animation
@@ -34,17 +35,16 @@ func _physics_process(delta):
 		velocity.x = walk_speed + 200
 		sprite.play("walk")  # Play walk animation
 		sprite.flip_h = false
-		
-	if jump_counter <= max_jumps and Input.is_action_just_pressed('ui_up'):
+
+	if jump_counter <= max_jumps and Input.is_action_just_pressed("ui_up"):
 		velocity.y = jump_speed
 		jump_counter += 1
 		sprite.play("jump")
-	elif Input.is_action_pressed('ui_up'):
+	elif Input.is_action_pressed("ui_up"):
 		sprite.play("jump")
 	elif is_on_floor() and Input.is_action_pressed("ui_down"):
 		velocity.y = velocity.y + 100
 		sprite.play("crouch")
-	
 
 	# "move_and_slide" already takes delta time into account.
 	move_and_slide()
